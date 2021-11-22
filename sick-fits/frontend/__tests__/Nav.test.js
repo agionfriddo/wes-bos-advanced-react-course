d639876c4aff2e1452058d7baf23d74b61f462ac
Andrew Gionfriddo <andrew@betterpt.com>
2021-11-22 10:15:18 -0500

    Add a couple more tests

--- sick-fits/frontend/__tests__/Nav.test.js
+++ sick-fits/frontend/__tests__/Nav.test.js
@@ -7,78 +7,78 @@ import Nav from '../components/Nav';
 
 // Make some mocks for being logged out, logged in, and logged in with cart items;
 const notSignedInMocks = [
-	{
-		request: { query: CURRENT_USER_QUERY },
-		result: { data: { authenticatedItem: null } },
-	},
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { authenticatedItem: null } },
+  },
 ];
 
 const signedInMocks = [
-	{
-		request: { query: CURRENT_USER_QUERY },
-		result: { data: { authenticatedItem: fakeUser() } },
-	},
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { authenticatedItem: fakeUser() } },
+  },
 ];
 
 const signedInMocksWithCartItems = [
-	{
-		request: { query: CURRENT_USER_QUERY },
-		result: {
-			data: {
-				authenticatedItem: fakeUser({
-					cart: [fakeCartItem()],
-				}),
-			},
-		},
-	},
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        authenticatedItem: fakeUser({
+          cart: [fakeCartItem()],
+        }),
+      },
+    },
+  },
 ];
 
 describe('<Nav />', () => {
-	it('Renders a minimal nav when signed out', () => {
-		const { container, debug } = render(
-			<CartStateProvider>
-				<MockedProvider mocks={notSignedInMocks}>
-					<Nav />
-				</MockedProvider>
-			</CartStateProvider>
-		);
-		expect(container).toHaveTextContent('Sign In');
-		expect(container).toMatchSnapshot();
-		const link = screen.getByText('Sign In');
-		expect(link).toHaveAttribute('href', '/signin');
-		const productsLink = screen.getByText('Products');
-		expect(productsLink).toHaveAttribute('href', '/products');
-	});
+  it('Renders a minimal nav when signed out', () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={notSignedInMocks}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    expect(container).toHaveTextContent('Sign In');
+    expect(container).toMatchSnapshot();
+    const link = screen.getByText('Sign In');
+    expect(link).toHaveAttribute('href', '/signin');
+    const productsLink = screen.getByText('Products');
+    expect(productsLink).toHaveAttribute('href', '/products');
+  });
 
-	it('renders full nav when signed in', async () => {
-		const { container, debug } = render(
-			<CartStateProvider>
-				<MockedProvider
-					mocks={signedInMocks}
-					defaultOptions={{ watchQuery: { fetchPolicy: 'no-cache' } }}
-				>
-					<Nav />
-				</MockedProvider>
-			</CartStateProvider>
-		);
-		await screen.findByText('Account');
-		expect(container).toMatchSnapshot();
-		expect(container).toHaveTextContent('Sign Out');
-		expect(container).toHaveTextContent('My Cart');
-	});
+  it('renders full nav when signed in', async () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider
+          mocks={signedInMocks}
+          defaultOptions={{ watchQuery: { fetchPolicy: 'no-cache' } }}
+        >
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    await screen.findByText('Account');
+    expect(container).toMatchSnapshot();
+    expect(container).toHaveTextContent('Sign Out');
+    expect(container).toHaveTextContent('My Cart');
+  });
 
-	it('renders the amount of items in the cart', async () => {
-		const { container, debug } = render(
-			<CartStateProvider>
-				<MockedProvider
-					mocks={signedInMocksWithCartItems}
-					defaultOptions={{ watchQuery: { fetchPolicy: 'no-cache' } }}
-				>
-					<Nav />
-				</MockedProvider>
-			</CartStateProvider>
-		);
-		await screen.findByText('Account');
-		expect(screen.getByText('3')).toBeInTheDocument();
-	});
+  it('renders the amount of items in the cart', async () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider
+          mocks={signedInMocksWithCartItems}
+          defaultOptions={{ watchQuery: { fetchPolicy: 'no-cache' } }}
+        >
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    await screen.findByText('Account');
+    expect(screen.getByText('3')).toBeInTheDocument();
+  });
 });
